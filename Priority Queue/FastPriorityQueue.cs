2ce03a2997323c44e7bcd7b2085c8f6bfe7b78c5
Ger Hobbelt <ger@hobbelt.com>
2021-03-31 19:07:49 +0200

    removed old cruft (4.5 and older)

--- Priority Queue/FastPriorityQueue.cs
+++ Priority Queue/FastPriorityQueue.cs
@@ -61,9 +61,6 @@ namespace Priority_Queue
         /// Removes every node from the queue.
         /// O(n) (So, don't do this often!)
         /// </summary>
-#if NET_VERSION_4_5
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
         public void Clear()
         {
             Array.Clear(_nodes, 1, _numNodes);
@@ -75,9 +72,6 @@ namespace Priority_Queue
         /// If node is or has been previously added to another queue, the result is undefined unless oldQueue.ResetNode(node) has been called
         /// O(1)
         /// </summary>
-#if NET_VERSION_4_5
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
         public bool Contains(T node)
         {
 #if DEBUG
@@ -105,9 +99,6 @@ namespace Priority_Queue
         /// If node is or has been previously added to another queue, the result is undefined unless oldQueue.ResetNode(node) has been called
         /// O(log n)
         /// </summary>
-#if NET_VERSION_4_5
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
         public void Enqueue(T node, float priority)
         {
 #if DEBUG
@@ -137,9 +128,6 @@ namespace Priority_Queue
             CascadeUp(node);
         }
 
-#if NET_VERSION_4_5
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
         private void CascadeUp(T node)
         {
             //aka Heapify-up
@@ -177,9 +165,6 @@ namespace Priority_Queue
             _nodes[node.QueueIndex] = node;
         }
 
-#if NET_VERSION_4_5
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
         private void CascadeDown(T node)
         {
             //aka Heapify-down
@@ -320,9 +305,6 @@ namespace Priority_Queue
         /// Returns true if 'higher' has higher priority than 'lower', false otherwise.
         /// Note that calling HasHigherPriority(node, node) (ie. both arguments the same node) will return false
         /// </summary>
-#if NET_VERSION_4_5
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
         private bool HasHigherPriority(T higher, T lower)
         {
             return (higher.Priority < lower.Priority);
@@ -332,9 +314,6 @@ namespace Priority_Queue
         /// Returns true if 'higher' has higher priority than 'lower', false otherwise.
         /// Note that calling HasHigherOrEqualPriority(node, node) (ie. both arguments the same node) will return true
         /// </summary>
-#if NET_VERSION_4_5
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
         private bool HasHigherOrEqualPriority(T higher, T lower)
         {
             return (higher.Priority <= lower.Priority);
@@ -345,9 +324,6 @@ namespace Priority_Queue
         /// If queue is empty, result is undefined
         /// O(log n)
         /// </summary>
-#if NET_VERSION_4_5
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
         public T Dequeue()
         {
 #if DEBUG
@@ -435,9 +411,6 @@ namespace Priority_Queue
         /// Calling this method on a node not in the queue results in undefined behavior
         /// O(log n)
         /// </summary>
-#if NET_VERSION_4_5
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
         public void UpdatePriority(T node, float priority)
         {
 #if DEBUG
@@ -459,9 +432,6 @@ namespace Priority_Queue
             OnNodeUpdated(node);
         }
 
-#if NET_VERSION_4_5
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
         private void OnNodeUpdated(T node)
         {
             //Bubble the updated node up or down as appropriate
@@ -483,9 +453,6 @@ namespace Priority_Queue
         /// If the node is not in the queue, the result is undefined.  If unsure, check Contains() first
         /// O(log n)
         /// </summary>
-#if NET_VERSION_4_5
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
         public void Remove(T node)
         {
 #if DEBUG
@@ -527,9 +494,6 @@ namespace Priority_Queue
         /// If you need to do this, please call originalQueue.ResetNode(node) before attempting to add it in the new queue
         /// If the node is currently in the queue or belongs to another queue, the result is undefined
         /// </summary>
-#if NET_VERSION_4_5
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
         public void ResetNode(T node)
         {
 #if DEBUG
@@ -554,13 +518,8 @@ namespace Priority_Queue
 
         public IEnumerator<T> GetEnumerator()
         {
-#if NET_VERSION_4_5 // ArraySegment does not implement IEnumerable before 4.5
-            IEnumerable<T> e = new ArraySegment<T>(_nodes, 1, _numNodes);
-            return e.GetEnumerator();
-#else
             for (int i = 1; i <= _numNodes; i++)
                 yield return _nodes[i];
-#endif
         }
 
         IEnumerator IEnumerable.GetEnumerator()
