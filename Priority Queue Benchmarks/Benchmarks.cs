70db62cce61edda2276ab9f35d89b76625d9a820
Ger Hobbelt <ger@hobbelt.com>
2021-03-31 18:29:14 +0200

    updated all packages

--- Priority Queue Benchmarks/Benchmarks.cs
+++ Priority Queue Benchmarks/Benchmarks.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 using Priority_Queue;
 
 namespace Priority_Queue_Benchmarks
