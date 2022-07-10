cb4cb2a88087a7dc97b1f389c84cb857654feb0c
stardeath <stardeath@hotmail.fr>
2022-07-10 21:14:19 +0200

    some cleanup

--- Priority Queue/FastPriorityQueueNode.cs
+++ Priority Queue/FastPriorityQueueNode.cs
@@ -2,28 +2,28 @@
 
 namespace Priority_Queue
 {
-    public class FastPriorityQueueNode<TPriority>
-    {
-        /// <summary>
-        /// The Priority to insert this node at.
-        /// Cannot be manually edited - see queue.Enqueue() and queue.UpdatePriority() instead
-        /// </summary>
-        public TPriority Priority { get; protected internal set; }
+	public class FastPriorityQueueNode<TPriority>
+	{
+		/// <summary>
+		/// The Priority to insert this node at.
+		/// Cannot be manually edited - see queue.Enqueue() and queue.UpdatePriority() instead
+		/// </summary>
+		public TPriority Priority { get; protected internal set; }
 
-        /// <summary>
-        /// Represents the current position in the queue
-        /// </summary>
-        public int QueueIndex { get; internal set; }
+		/// <summary>
+		/// Represents the current position in the queue
+		/// </summary>
+		public int QueueIndex { get; internal set; }
 
 #if DEBUG
-        /// <summary>
-        /// The queue this node is tied to. Used only for debug builds.
-        /// </summary>
-        public object Queue { get; internal set; }
+		/// <summary>
+		/// The queue this node is tied to. Used only for debug builds.
+		/// </summary>
+		public object Queue { get; internal set; }
 #endif
-    }
+	}
 
-    public class FastPriorityQueueNode : FastPriorityQueueNode<float>
-    {
-    }
+	public class FastPriorityQueueNode : FastPriorityQueueNode<float>
+	{
+	}
 }
--- Priority Queue/StablePriorityQueueNode.cs
+++ Priority Queue/StablePriorityQueueNode.cs
@@ -1,15 +1,14 @@
 ﻿namespace Priority_Queue
 {
-    public class StablePriorityQueueNode<TPriority> : FastPriorityQueueNode<TPriority>
-    {
-        /// <summary>
-        /// Represents the order the node was inserted in
-        /// </summary>
-        public long InsertionIndex { get; internal set; }
-    }
+	public class StablePriorityQueueNode<TPriority> : FastPriorityQueueNode<TPriority>
+	{
+		/// <summary>
+		/// Represents the order the node was inserted in
+		/// </summary>
+		public long InsertionIndex { get; internal set; }
+	}
 
-
-    public class StablePriorityQueueNode : StablePriorityQueueNode<float>
-    {
-    }
+	public class StablePriorityQueueNode : StablePriorityQueueNode<float>
+	{
+	}
 }
